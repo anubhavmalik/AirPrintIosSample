8c807f1009771eb767fc8c0ad38a3382ac04ed13
anubhavmalik <anubhavmalikdeveloper@bitbucket.org>
2018-12-30 15:01:26 +0530

    Initial Commit

---
+++ iLivPrintIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iLivPrintIos
+//
+//  Created by Anubhav Malik on 30/12/18.
+//  Copyright © 2018 Anubhav Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iLivPrintIosTests/iLivPrintIosTests.swift
@@ -0,0 +1,34 @@
+//
+//  iLivPrintIosTests.swift
+//  iLivPrintIosTests
+//
+//  Created by Anubhav Malik on 30/12/18.
+//  Copyright © 2018 Anubhav Malik. All rights reserved.
+//
+
+import XCTest
+@testable import iLivPrintIos
+
+class iLivPrintIosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
